895a0f33cc67ba1347303f61b4af2e74555ae5a3
ImperialBreakCode <eeeda522@gmail.com>
2025-08-31 13:21:03 +0300

    adding user created domain event and handlers

---
+++ src/Backend/src/Services/Accounts/API.Accounts.Application/Features/Users/DomainEventHandlers/UserCreated/VerifyEmailHandler.cs
@@ -0,0 +1,37 @@
+﻿using API.Accounts.Domain;
+using API.Accounts.Domain.Aggregates.UserAggregate.DomainEvents;
+using API.Shared.Application.Contracts.Accounts.Commands;
+using MassTransit;
+using MediatR;
+
+namespace API.Accounts.Application.Features.Users.DomainEventHandlers.UserCreated
+{
+    public class VerifyEmailHandler : INotificationHandler<UserCreatedDomainEvent>
+    {
+        private readonly IPublishEndpoint _endpoint;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VerifyEmailHandler(IPublishEndpoint endpoint, IUnitOfWork unitOfWork)
+        {
+            _endpoint = endpoint;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO: create and save token
+
+            var user = _unitOfWork.UserRepository.GetActiveEntityById(notification.UserId);
+
+            if (user is null)
+            {
+                return;
+            }
+
+            var email = user.Emails.First(x => x.IsPrimary).Email;
+            var testToken = "sometoken";
+
+            await _endpoint.Publish(new SendEmailVerification(email, testToken));
+        }
+    }
+}
---
+++ src/Backend/src/Services/Accounts/API.Accounts.Domain/Aggregates/UserAggregate/DomainEvents/UserCreatedDomainEvent.cs
@@ -0,0 +1,6 @@
+﻿using API.Shared.Domain.Interfaces;
+
+namespace API.Accounts.Domain.Aggregates.UserAggregate.DomainEvents
+{
+    public record UserCreatedDomainEvent(string UserId) : IDomainEvent;
+}
