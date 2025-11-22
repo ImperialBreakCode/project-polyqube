550899454ffa46fdfd1c3b209646f606ed5efb08
ImperialBreakCode <eeeda522@gmail.com>
2025-11-22 22:36:54 +0200

    add admin app

---
+++ src/frontend/apps/admin/eslint.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "eslint/config";
+
+import { nextJsConfig as globalNextJsConfig } from "@repo/eslint-config/next-js";
+
+const eslintConfig = defineConfig([...globalNextJsConfig]);
+
+export default eslintConfig;
