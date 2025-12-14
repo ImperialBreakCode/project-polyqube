96298428d23bee50cfc07c162da64a83ce1c980c
ImperialBreakCode <eeeda522@gmail.com>
2025-12-14 20:24:03 +0200

    adding logo, adding nav menu overall structure and look

--- src/frontend/apps/web/src/app/(landingArea)/layout.tsx
+++ src/frontend/apps/web/src/app/(landingArea)/layout.tsx
@@ -1,9 +1,38 @@
+import Image from 'next/image';
 import { ReactNode } from 'react';
+import logoSVG from '@/assets/logo.svg';
+import Link from 'next/link';
 
 function LandingLayout({ children }: { children: ReactNode }) {
 	return (
 		<div>
-			<header></header>
+			<header>
+				<div className='fixed z-20 text-white'>
+					<nav className='m-10 flex flex-row justify-center'>
+						<div className='me-30'>
+							<Image
+								width={30}
+								height={30}
+								src={logoSVG}
+								alt='polyqube logo'
+							/>
+						</div>
+						<div>
+							<ul className='flex h-full flex-row justify-center space-x-20'>
+								<li>
+									<Link href={'#'}>menu 1</Link>
+								</li>
+								<li>
+									<Link href={'#'}>menu 2</Link>
+								</li>
+								<li>
+									<Link href={'#'}>menu 3</Link>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
 			<main>{children}</main>
 		</div>
 	);
