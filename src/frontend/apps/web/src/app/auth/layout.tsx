f5173a07f3a8ee212a2117b77ecf465cfb835a77
ImperialBreakCode <eeeda522@gmail.com>
2026-02-15 17:00:43 +0200

    floating lines fixes

--- src/frontend/apps/web/src/app/auth/layout.tsx
+++ src/frontend/apps/web/src/app/auth/layout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
+import FloatingLines from '@repo/ui/components/FloatingLines';
 
 function AuthLayout({ children }: { children: ReactNode }) {
 	return (
 		<div className='h-screen p-2 bg-black text-white'>
 			<div className='flex h-full space-x-2'>
-				<div className='bg-amber-900 flex-1 rounded-md'></div>
+				<div className='flex-1 rounded-md'>
+					<FloatingLines lineCount={5} lineDistance={15} />
+				</div>
 				<div className='bg-[#202020] flex-1 rounded-md'>{children}</div>
 			</div>
 		</div>
--- src/frontend/packages/ui/src/components/index.ts
+++ src/frontend/packages/ui/src/components/index.ts
@@ -1,1 +1,3 @@
+export * from './FloatingLines';
+
 export * from './ui';
