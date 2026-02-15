d687a52ae49a24b0af5b6e788478a4b6bcd9d286
ImperialBreakCode <eeeda522@gmail.com>
2026-02-15 15:59:04 +0200

    implement layout overall look

--- src/frontend/apps/web/src/app/auth/layout.tsx
+++ src/frontend/apps/web/src/app/auth/layout.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from 'react';
 
 function AuthLayout({ children }: { children: ReactNode }) {
-	return <div>{children}</div>;
+	return (
+		<div className='h-screen p-2 bg-black text-white'>
+			<div className='flex h-full space-x-2'>
+				<div className='bg-amber-900 flex-1 rounded-md'></div>
+				<div className='bg-[#202020] flex-1 rounded-md'>{children}</div>
+			</div>
+		</div>
+	);
 }
 
 export default AuthLayout;
