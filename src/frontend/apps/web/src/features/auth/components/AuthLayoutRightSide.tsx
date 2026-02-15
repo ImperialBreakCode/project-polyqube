93c65885f67d8dab9beb1276e7dc816389470b36
ImperialBreakCode <eeeda522@gmail.com>
2026-02-15 17:28:51 +0200

    auth pages title

--- src/frontend/apps/web/src/features/auth/components/AuthLayoutRightSide.tsx
+++ src/frontend/apps/web/src/features/auth/components/AuthLayoutRightSide.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import FloatingLines from '@repo/ui/components/FloatingLines';
 
 const AuthLayoutRightSide = () => {
+	const pathname = usePathname();
+
 	return (
 		<>
-			<h1 className='text-7xl absolute bottom-7 left-7'>Auth Title</h1>
+			<h1
+				className='text-7xl absolute bottom-15 left-8 capitalize
+					font-light'
+			>
+				{pathname.split('/').splice(-1)}
+			</h1>
 			<FloatingLines lineCount={5} lineDistance={15} />
 		</>
 	);
