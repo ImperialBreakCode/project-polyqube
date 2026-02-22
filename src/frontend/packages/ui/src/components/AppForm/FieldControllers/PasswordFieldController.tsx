7d296c68d98618ea75aaba7b639b35ec92f5dba5
ImperialBreakCode <eeeda522@gmail.com>
2026-02-22 22:11:45 +0200

    change password field

--- src/frontend/packages/ui/src/components/AppForm/FieldControllers/PasswordFieldController.tsx
+++ src/frontend/packages/ui/src/components/AppForm/FieldControllers/PasswordFieldController.tsx
@@ -1,7 +1,45 @@
-'use client';
+import { Controller, FieldValues } from 'react-hook-form';
+import useExtendedAppFormContext from '../../../hooks/useExtendedAppFormContext';
+import { cn } from '@repo/ui/lib/utils';
+import { BasicFieldWrapper } from '../FieldWrappers';
+import { PasswordField } from '../../Fields';
+import { BasicControllerProps } from './createBasicController';
+import { generateControllerId } from './utils';
 
-import PasswordField from '@repo/ui/components/Fields/PasswordField';
-import createBasicController from './createBasicController';
+const PasswordFieldController = <T extends FieldValues>({
+	name,
+	label,
+	placeholder,
+	className,
+	fieldClassName,
+	disabled = false,
+}: BasicControllerProps<T>) => {
+	const { formId, control } = useExtendedAppFormContext();
+	const id = generateControllerId(formId, name);
 
-const PasswordFieldController = createBasicController(PasswordField);
+	return (
+		<Controller
+			name={name}
+			control={control}
+			render={({ field, fieldState }) => (
+				<BasicFieldWrapper
+					className={className}
+					htmlFor={id}
+					fieldState={fieldState}
+					labelText={label}
+				>
+					<PasswordField
+						{...field}
+						id={id}
+						className={cn(fieldClassName, 'pr-2')}
+						aria-invalid={fieldState.invalid}
+						placeholder={placeholder}
+						disabled={disabled}
+					/>
+				</BasicFieldWrapper>
+			)}
+		/>
+	);
+};
+
 export default PasswordFieldController;
--- src/frontend/packages/ui/src/components/Fields/PasswordField.tsx
+++ src/frontend/packages/ui/src/components/Fields/PasswordField.tsx
@@ -1,5 +1,29 @@
-import createBasicInputField from './createBasicInputField';
+'use client';
 
-const PasswordField = createBasicInputField('password');
+import { useState } from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import { Input } from '../ui/Input';
+import { BasicInputFieldProps } from './types';
+
+const PasswordField = (props: BasicInputFieldProps) => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	return (
+		<div className='relative'>
+			<Input {...props} type={isVisible ? 'text' : 'password'} />
+			<button
+				type='button'
+				onClick={() => setIsVisible((prevState) => !prevState)}
+				className='text-muted-foreground absolute top-1/2
+					-translate-y-1/2 right-2 hover:bg-[#ffffff26] rounded p-1'
+			>
+				{isVisible ? <EyeOffIcon size={17} /> : <EyeIcon size={17} />}
+				<span className='sr-only'>
+					{isVisible ? 'Hide password' : 'Show password'}
+				</span>
+			</button>
+		</div>
+	);
+};
 
 export default PasswordField;
