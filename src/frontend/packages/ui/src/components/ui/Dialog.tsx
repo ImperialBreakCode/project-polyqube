95f1991dab73763cd4055756783a87f9c53775a8
ImperialBreakCode <eeeda522@gmail.com>
2026-03-03 21:53:43 +0200

    fixes

--- src/frontend/packages/ui/src/components/ui/Dialog.tsx
+++ src/frontend/packages/ui/src/components/ui/Dialog.tsx
@@ -47,7 +47,6 @@ const DialogContent = React.forwardRef<
 				p-6 shadow-lg duration-200 data-[state=open]:animate-in
 				data-[state=closed]:animate-out data-[state=closed]:fade-out-0
 				data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95
-				data-[state=open]:zoom-in-95
 				data-[state=closed]:slide-out-to-top-[48%]
 				data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg`,
 				className,
