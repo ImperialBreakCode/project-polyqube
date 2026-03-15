9331044feb4baac371f2053ea02ce0ca2d5835be
ImperialBreakCode <eeeda522@gmail.com>
2026-03-15 18:34:51 +0200

    add logout BE endpoint

---
+++ src/Backend/src/Services/Accounts/API.Accounts.Application/Features/Users/Commands/RevokeSession/RevokeSessionCommand.cs
@@ -0,0 +1,6 @@
+﻿using API.Shared.Application.Interfaces;
+
+namespace API.Accounts.Application.Features.Users.Commands.RevokeSession
+{
+    public record RevokeSessionCommand(string UserId, string SessionId) : ICommand;
+}
