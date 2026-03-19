468bc75e47957b83d618cd68ba4404224aaef548
ImperialBreakCode <eeeda522@gmail.com>
2026-03-19 20:41:09 +0200

    add guest proxy

--- src/frontend/apps/web/src/proxy.ts
+++ src/frontend/apps/web/src/proxy.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import AuthProxy from './proxy/Auth.proxy';
+import GuestProxy from './proxy/Guest.proxy';
 
-const proxies = [new AuthProxy()];
+const proxies = [new AuthProxy(), new GuestProxy()];
 
 export async function proxy(request: NextRequest) {
 	for (const proxy of proxies) {
