3e51a45aab7c021ac4090acad5a68d3ce3588174
ImperialBreakCode <eeeda522@gmail.com>
2026-03-21 18:47:38 +0200

    add sidebar functionality

--- src/frontend/apps/web/src/shared/elements/UserPanelSidebar/SidebarUser.tsx
+++ src/frontend/apps/web/src/shared/elements/UserPanelSidebar/SidebarUser.tsx
@@ -1,4 +1,6 @@
-import { BadgeCheck, Bell, CreditCard, Ellipsis, LogOut } from 'lucide-react';
+'use client';
+
+import { Ellipsis, Home, LogOut } from 'lucide-react';
 import {
 	SidebarMenu,
 	SidebarMenuButton,
@@ -18,19 +20,38 @@ import {
 	AvatarFallback,
 	AvatarImage,
 } from '@repo/ui/components/ui/Avatar';
+import { useCurrentUser, useLogout } from '@/shared/api';
+import Link from 'next/link';
+import { ROUTE_PATHS } from '@/shared/constants';
+import { useRouter } from 'next/navigation';
 
 const SidebarUser = () => {
+	const router = useRouter();
+	const { logout, logoutLoading } = useLogout();
+	const { currentUser } = useCurrentUser();
+
 	const avatar = (
 		<Avatar className='h-8 w-8 rounded-lg'>
-			<AvatarImage src={'...'} alt={'alt text'} />
-			<AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+			<AvatarImage
+				src={currentUser?.userDetails?.profilePicturePath}
+				alt={'alt text'}
+			/>
+			<AvatarFallback className='rounded-lg uppercase'>
+				{currentUser?.userDetails?.firstName[0]}
+				{currentUser?.userDetails?.lastName[0]}
+			</AvatarFallback>
 		</Avatar>
 	);
 
 	const userInfo = (
 		<div className='grid flex-1 text-left text-sm leading-tight'>
-			<span className='truncate font-medium'>name name</span>
-			<span className='truncate text-xs'>email@email.mail</span>
+			<span className='truncate font-medium'>
+				{currentUser?.userDetails?.firstName}{' '}
+				{currentUser?.userDetails?.lastName}
+			</span>
+			<span className='truncate text-xs'>
+				{currentUser?.emails.find((x) => x.isPrimary)?.email}
+			</span>
 		</div>
 	);
 
@@ -42,7 +63,8 @@ const SidebarUser = () => {
 						<SidebarMenuButton
 							size='lg'
 							className='data-[state=open]:bg-sidebar-accent
-								data-[state=open]:text-sidebar-accent-foreground'
+								data-[state=open]:text-sidebar-accent-foreground
+								cursor-pointer'
 						>
 							{avatar}
 							{userInfo}
@@ -67,21 +89,21 @@ const SidebarUser = () => {
 						</DropdownMenuLabel>
 						<DropdownMenuSeparator />
 						<DropdownMenuGroup>
-							<DropdownMenuItem>
-								<BadgeCheck />
-								Account
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<CreditCard />
-								Billing
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<Bell />
-								Notifications
+							<DropdownMenuItem asChild>
+								<Link href={ROUTE_PATHS.home}>
+									<Home />
+									Landing Area
+								</Link>
 							</DropdownMenuItem>
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>
+						<DropdownMenuItem
+							disabled={logoutLoading}
+							onClick={async () => {
+								await logout();
+								router.push(ROUTE_PATHS.home);
+							}}
+						>
 							<LogOut />
 							Log out
 						</DropdownMenuItem>
