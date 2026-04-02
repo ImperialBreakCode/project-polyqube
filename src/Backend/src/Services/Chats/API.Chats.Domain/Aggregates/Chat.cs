1a9b4360c17615ecbdd8ce0221df40f4019c5c4d
ImperialBreakCode <eeeda522@gmail.com>
2026-04-02 19:44:20 +0300

    add the rest of the message domain models

---
+++ src/Backend/src/Services/Chats/API.Chats.Domain/Aggregates/Chat.cs
@@ -0,0 +1,19 @@
+﻿using API.Shared.Domain.Base;
+
+namespace API.Chats.Domain.Aggregates
+{
+    public class Chat: AuditableSoftDeleteAggregateRoot
+    {
+        private Chat()
+        {
+        }
+
+        public bool IsGroupChat { get; set; }
+        public bool AIEnabled { get; set; }
+
+        public static Chat Create()
+        {
+            return new Chat();
+        }
+    }
+}
---
+++ src/Backend/src/Services/Chats/API.Chats.Domain/Aggregates/Participant.cs
@@ -0,0 +1,40 @@
+﻿using API.Chats.Domain.Aggregates.UserProfilesAggregate;
+using API.Shared.Domain.Base;
+
+namespace API.Chats.Domain.Aggregates
+{
+    public class Participant: AuditableSoftDeleteAggregateRoot
+    {
+        private Participant()
+        {
+        }
+
+        private Participant(string chatId, string? userProfileId, string? agentId)
+        {
+            ChatId = chatId;
+            UserProfileId = userProfileId;
+            ChatAgentid = agentId;
+        }
+
+        public string? ChatNickname { get; set; }
+
+        public string? UserProfileId { get; set; }
+        public UserProfile? UserProfile { get; set; }
+
+        public string ChatId { get; set; }
+        public Chat Chat { get; set; }
+
+        public string? ChatAgentid { get; set; }
+        public ChatAgent? ChatAgent { get; set; }
+
+        public static Participant CreateUserParticipant(string chatId, string userProfileId)
+        {
+            return new Participant(chatId, userProfileId, null);
+        }
+
+        public static Participant CreateAgentParticipant(string chatId, string agentId)
+        {
+            return new Participant(chatId, null, agentId);
+        }
+    }
+}
