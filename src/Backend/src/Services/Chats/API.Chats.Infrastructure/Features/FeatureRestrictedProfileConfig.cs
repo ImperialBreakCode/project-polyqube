aead311cbf35044a89b0f1376493c42ac8a29f5f
ImperialBreakCode <eeeda522@gmail.com>
2026-04-04 17:41:02 +0300

    add chat feature configs

---
+++ src/Backend/src/Services/Chats/API.Chats.Infrastructure/Features/FeatureRestrictedProfileConfig.cs
@@ -0,0 +1,26 @@
+﻿using API.Chats.Domain.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Chats.Infrastructure.Features
+{
+    internal class FeatureRestrictedProfileConfig : IEntityTypeConfiguration<FeatureRestrictedProfile>
+    {
+        public void Configure(EntityTypeBuilder<FeatureRestrictedProfile> builder)
+        {
+            builder.ToTable("feature_restricted_user");
+
+            builder.HasOne(fru => fru.RestrictedProfile)
+                .WithMany()
+                .HasForeignKey(fru => fru.RestrictedProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(fru => fru.ChatFeature)
+                .WithMany()
+                .HasForeignKey(fru => fru.ChatFeatureId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasKey(ur => new { ur.RestrictedProfileId, ur.ChatFeatureId });
+        }
+    }
+}
