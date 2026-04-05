ec29642281e1a27251ac0c78f3afebd5a2b93d45
ImperialBreakCode <eeeda522@gmail.com>
2026-04-05 14:13:56 +0300

    add agent repo

---
+++ src/Backend/src/Services/Chats/API.Chats.Common/Features/ChatAgents/Exceptions/AgentUsernameAlreadyExists.cs
@@ -0,0 +1,13 @@
+﻿using API.Shared.Common.Exceptions;
+
+namespace API.Chats.Common.Features.ChatAgents.Exceptions
+{
+    public class AgentUsernameAlreadyExists : BadRequestException
+    {
+        private const string MESSAGE = "This agent username is already taken";
+
+        public AgentUsernameAlreadyExists() : base(MESSAGE)
+        {
+        }
+    }
+}
---
+++ src/Backend/src/Services/Chats/API.Chats.Infrastructure/Features/ChatAgents/ChatAgentRepository.cs
@@ -0,0 +1,30 @@
+﻿using API.Chats.Common.Features.ChatAgents.Exceptions;
+using API.Chats.Domain.Aggregates;
+using API.Chats.Domain.Repositories;
+using API.Shared.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Chats.Infrastructure.Features.ChatAgents
+{
+    internal class ChatAgentRepository : InsertReadRepository<ChatAgent>, IChatAgentRepository
+    {
+        public ChatAgentRepository(DbSet<ChatAgent> dbSet) : base(dbSet)
+        {
+        }
+
+        public async Task<ChatAgent?> GetByUsername(string username)
+        {
+            return await DbSet.FirstOrDefaultAsync(x => x.AgentUsername == username);
+        }
+
+        public override void Insert(ChatAgent entity)
+        {
+            if (DbSet.Any(x => x.AgentUsername == entity.AgentUsername))
+            {
+                throw new AgentUsernameAlreadyExists();
+            }
+
+            base.Insert(entity);
+        }
+    }
+}
