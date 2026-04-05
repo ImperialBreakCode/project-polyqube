97aae9648f6506f21b3cf21d19bbcb941412f355
ImperialBreakCode <eeeda522@gmail.com>
2026-04-05 15:36:33 +0300

    add chat feature repository

---
+++ src/Backend/src/Services/Chats/API.Chats.Domain/Repositories/IChatFeatureRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Chats.Domain.Aggregates;
+using API.Shared.Domain.Interfaces.Repo.BaseInterfaces;
+
+namespace API.Chats.Domain.Repositories
+{
+    public interface IChatFeatureRepository: IRepoInsert<ChatFeature>, IRepoRead<ChatFeature>
+    {
+        Task<ChatFeature?> GetByFeatureName(string name);
+    }
+}
---
+++ src/Backend/src/Services/Chats/API.Chats.Infrastructure/Features/ChatFeatures/ChatFeatureRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Chats.Domain.Aggregates;
+using API.Chats.Domain.Repositories;
+using API.Shared.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Chats.Infrastructure.Features.ChatFeatures
+{
+    internal class ChatFeatureRepository : InsertReadRepository<ChatFeature>, IChatFeatureRepository
+    {
+        public ChatFeatureRepository(DbSet<ChatFeature> dbSet) : base(dbSet)
+        {
+        }
+
+        public async Task<ChatFeature?> GetByFeatureName(string name)
+        {
+            return await DbSet.FirstOrDefaultAsync(x => x.FeatureName == name);
+        }
+    }
+}
