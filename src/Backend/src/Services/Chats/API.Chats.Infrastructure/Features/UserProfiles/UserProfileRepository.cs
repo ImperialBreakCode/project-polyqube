d5d3cb6732ca2011ae547b74f4c96b69a9fe2f63
ImperialBreakCode <eeeda522@gmail.com>
2026-04-05 13:56:01 +0300

    add user profile repository

---
+++ src/Backend/src/Services/Chats/API.Chats.Infrastructure/Features/UserProfiles/UserProfileRepository.cs
@@ -0,0 +1,36 @@
+﻿using API.Chats.Domain.Aggregates.UserProfilesAggregate;
+using API.Chats.Domain.Repositories;
+using API.Shared.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Chats.Infrastructure.Features.UserProfiles
+{
+    internal class UserProfileRepository : SoftDeleteRepository<UserProfile>, IUserProfileRepository
+    {
+        private readonly ChatDbContext _context;
+
+        public UserProfileRepository(ChatDbContext context) : base(context.UserProfile)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<BlockedProfile>> GetBlockedProfilesByBlockedProfileId(string profileId)
+        {
+            return await _context.BlockedProfiles
+                .AsNoTracking()
+                .Include(x => x.BlockedBy)
+                .Where(x => x.BlockedUserId == profileId)
+                .ToListAsync();
+        }
+
+        public async Task<UserProfile?> GetProfileByUserId(string userId, bool includeDeleted = default)
+        {
+            if (includeDeleted)
+            {
+                return await DbSet.FirstOrDefaultAsync(x => x.UserId == userId);
+            }
+
+            return await DbSet.FirstOrDefaultAsync(x => x.UserId == userId && x.DeletedAt == null);
+        }
+    }
+}
