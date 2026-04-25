80c10a2d5abf1cc4b269a537b18d1c60a1adc206
ImperialBreakCode <eeeda522@gmail.com>
2026-04-25 16:21:29 +0300

    add chat next app

---
+++ src/frontend/apps/chat/eslint.config.mjs
@@ -0,0 +1,9 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import { defineConfig } from 'eslint/config';
+import { nextJsConfig as globalNextJsConfig } from '@repo/eslint-config/next-js';
+
+const eslintConfig = defineConfig([
+	...globalNextJsConfig
+]);
+
+export default eslintConfig;
